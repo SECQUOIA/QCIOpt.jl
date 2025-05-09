10a4bf6a133296eee6974f0ea6875c773efe3b17
parkyr <park407@purdue.edu>
2025-05-09 15:14:32 -0400

    add additional attribute and functions for optimizer

--- src/MOI_wrapper/attributes.jl
+++ src/MOI_wrapper/attributes.jl
@@ -6,12 +6,25 @@ function MOI.get(solver::Optimizer{T}, ::MOI.SolverName) where {T}
     return "QCI Optimizer ($device_type)"
 end
 
-# [ ] SolverVersion	        Yes	No	No
-# [ ] RawSolver	            Yes	No	No
-# [ ] Silent	            Yes	Yes	Yes
-# [ ] TimeLimitSec	        Yes	Yes	Yes
-# [ ] RawOptimizerAttribute	Yes	Yes	Yes
-# [ ] NumberOfThreads	    Yes	Yes	Yes
+# [x] SolverVersion	        Yes	No	No
+function MOI.get(::Optimizer{T}, ::MOI.SolverVersion) where {T}
+    return v"4.5.0"     #qci-client version
+end
+
+### Check below for the list of attributes that are supported by the QCI Optimizer and create functions - YP 
+
+# [ ] RawSolver	            Yes	No	No   - maybe there is none, should return nothing if so (or the optimizer itself/solver variable)
+function MOI.get(::Optimizer{T}, ::MOI.RawSolver) where {T}
+    return nothing
+end
+
+
+# [ ] Silent	            Yes	Yes	Yes     - check on QCI on how to suppress output, return that it's not supported if not; 
+# [ ] TimeLimitSec	        Yes	Yes	Yes     - check on QCI on how long you allow the solver to run, if not, no support also; might be device dependent; may need to differentiate among the solvers- if tricky do last. 
+# [ ] RawOptimizerAttribute	Yes	Yes	Yes     - select optimizer based on string? skip for now 
+
+# [x] NumberOfThreads	    Yes	Yes	Yes  
+MOI.supports(::Optimizer{T}, ::MOI.NumberOfThreads) where {T} = false # thread is not configurable by the user 
 
 # Custom Attributes
 struct DeviceType <: MOI.AbstractOptimizerAttribute end
@@ -23,7 +36,7 @@ end
 @doc raw"""
     supports_device_type(device_type::AbstractString)::Bool
 
-Tells wether the solver supports a given `device_type`.
+Tells whether the solver supports a given `device_type`.
 """
 function supports_device_type(device_type::AbstractString)::Bool
     return device_type == "dirac-1" || device_type == "dirac-3"
